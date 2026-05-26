46c5edd5f5757ff871d004370ddf1153726e7fa7
ProgecktProf <sergey.nikishin555123123@gmail.com>
2026-05-26 11:09:04 +0300

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+
+  return (
+    <div style={{padding: 30}}>
+      <h1>Lead System</h1>
+      <p>Admin panel working</p>
+    </div>
+  )
+
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
